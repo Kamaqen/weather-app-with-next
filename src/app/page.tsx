5b67682250448bd4cb9ce532d09adbac5b415a5f
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-02-19 16:38:30 -0500

    ADD: react query with axios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,86 @@
+"use client";
+
+import axios from "axios";
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+import { useQuery } from "react-query";
+
+// https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=14694ec523d67ef5aa00e1231626915d
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherListItem[];
+  city: CityInfo;
+}
+
+interface WeatherListItem {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface CityInfo {
+  id: number;
+  name: string;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    "repoData",
+    async () => {
+      const { data } = await axios.get(
+        "https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=14694ec523d67ef5aa00e1231626915d&cnt=56"
+      );
+      return data;
+    }
+
+    // fetch(
+    //   "https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=14694ec523d67ef5aa00e1231626915d&cnt=56"
+    // ).then((res) => res.json())
+  );
+  console.log(data);
+
+  if (isLoading) return "Loading...";
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
