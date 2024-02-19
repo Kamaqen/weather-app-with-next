a3063fdc6bbdd1fafc51a00f7fa00d1a4c8e06b3
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-02-19 12:46:38 -0500

    ADD: yellow sun react icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdWbSunny } from "react-icons/md";
 
 type Props = {};
 
@@ -8,6 +9,7 @@ const Navbar = (props: Props) => {
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
         <p className="flex items-center justify-center gap-2">
           <h2 className="text-gray-500 text-3xl">Weather</h2>
+          <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
         </p>
       </div>
     </nav>
